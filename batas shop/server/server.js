fce2c5a46aa64e8bbc78a9277798f5badb314109
Sharef214 <m.nasit2021@gtu.edu.tr>
2024-10-29 21:35:38 +0300

    DATABASE
    creating the database

---
+++ batas shop/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+
+
+
+mongoose
+.connect('mongodb+srv://mserifnas21:1mEsoWzRRbhhU0Mq@batas.6mw8s.mongodb.net/')
+.then(() => console.log('MangoDB connected'))
+.catch((error) => console.log(error));
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content-type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Progma'
+        ],
+        credentials : true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, ()=> console.log('Server is now running on port ${PORT}'));
