8f478bc1e175b50f1606b29b636ef1d61b744a13
Sharef214 <m.nasit2021@gtu.edu.tr>
2024-10-30 19:39:48 +0300

    Update auth-controller.js

--- batas shop/server/controllers/auth/auth-controller.js
+++ batas shop/server/controllers/auth/auth-controller.js
@@ -13,18 +13,18 @@ const registerUser = async(req, res) => {
 
     try{
 
-        const hashPassword = await bycrypt.hash(password, 12);
+        const hashPassword = await bcrypt.hash(password, 12);
         const newUser = new User({
             userName, 
             email , 
             password : hashPassword,
-        })
+        });
 
         await newUser.save()
         res.status(200).json({
             success : true,
             message : "Registration successful ",
-        })
+        });
     }catch(e){
         console.log(e);
         res.status(500).json({
