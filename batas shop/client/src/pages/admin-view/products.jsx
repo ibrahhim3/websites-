a15da936e700ea4cb1ce54e271257a263465d909
ibr-al1 <i.al2020@gtu.edu.tr>
2024-11-06 16:21:54 +0300

    uplaoding images in porducts (admin)

--- batas shop/client/src/pages/admin-view/products.jsx
+++ batas shop/client/src/pages/admin-view/products.jsx
@@ -1,6 +1,63 @@
+import ProductImageUpload from "@/components/admin-view/image-uplaod";
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { Fragment, useState } from "react";
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+};
 function AdminProducts() {
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+  const [formData, setFormData] = useState(initialFormData);
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  function onSubmit() {}
+
   return (
-    <div> admin Products dddddddddddddddddddddddddddddddddddddfffddddddddddd </div>
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end ">
+        <Button
+          onClick={() => setOpenCreateProductsDialog(true)}
+          className="bg-black text-white"
+        >
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => {
+          setOpenCreateProductsDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto bg-white text-black">
+          <SheetHeader>
+            <SheetTitle>Add  new product</SheetTitle>
+          </SheetHeader>
+          <ProductImageUpload imageFile={imageFile}
+            setImageFile={setImageFile}
+            uploadedImageUrl={uploadedImageUrl}
+            setUploadedImageUrl={setUploadedImageUrl}/>
+          <div className="py-6 ">
+            <CommonForm  
+              onSubmit={onSubmit}
+              formData={formData}
+              setFormData={setFormData}
+              buttonText="Add"
+              formControls={addProductFormElements}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
   );
 }
 
