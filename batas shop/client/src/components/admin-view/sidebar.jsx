54e5a4296a2dca93373eafb7fa506eb2800b605e
ibr-al1 <i.al2020@gtu.edu.tr>
2024-11-06 12:32:24 +0300

    .

--- batas shop/client/src/components/admin-view/sidebar.jsx
+++ batas shop/client/src/components/admin-view/sidebar.jsx
@@ -6,30 +6,30 @@ import {
 } from "lucide-react";
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
-import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet"; // Adjust as per your project structure
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
 const adminSidebarMenuItems = [
   {
-    id: "dashbaord",
-    label: "dashboard",
+    id: "dashboard",
+    label: "Dashboard",
     path: "/admin/dashboard",
     icon: <LayoutDashboard />,
   },
   {
     id: "products",
-    label: "products",
+    label: "Products",
     path: "/admin/products",
     icon: <ShoppingBasket />,
   },
   {
     id: "orders",
-    label: "orders",
+    label: "Orders",
     path: "/admin/orders",
     icon: <BadgeCheck />,
   },
 ];
 
-function MenuItems() {
+function MenuItems({ setOpen }) {
   const navigate = useNavigate();
 
   return (
@@ -39,8 +39,9 @@ function MenuItems() {
           key={menuItem.id}
           onClick={() => {
             navigate(menuItem.path);
+            setOpen ? setOpen(false) : null;
           }}
-          className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-gray-600 hover:bg-gray-200 hover:text-black"
+          className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground"
         >
           {menuItem.icon}
           <span>{menuItem.label}</span>
@@ -56,31 +57,30 @@ function AdminSideBar({ open, setOpen }) {
   return (
     <Fragment>
       <Sheet open={open} onOpenChange={setOpen}>
-        <SheetContent side="left" className="w-64 bg-white ">
-          <div className="flex flex-col h-full">
-            <SheetHeader className="border-b ">
-              <SheetTitle className="flex gap-2 mt-5 mb-5 ">
+        <SheetContent side="left" className="w-64  bg-white">
+          <div className="flex flex-col h-full  bg-white">
+            <SheetHeader className="border-b  ">
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
                 <ChartNoAxesCombined size={30} />
-                <h1 className="text-2xl font-extrabold text-black ">
-                  Admin Panel
-                </h1>
+                <h1 className="text-2xl font-extrabold">Admin Panel</h1>
               </SheetTitle>
             </SheetHeader>
-            <MenuItems />
+            <MenuItems setOpen={setOpen} />
           </div>
         </SheetContent>
       </Sheet>
-      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex ">
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
         <div
           onClick={() => navigate("/admin/dashboard")}
-          className="flex cursor-pointer items-center gap-2 "
+          className="flex cursor-pointer items-center gap-2"
         >
           <ChartNoAxesCombined size={30} />
-          <h1 className="text-2xl font-extrabold ">Admin Panel</h1>
+          <h1 className="text-2xl font-extrabold">Admin Panel</h1>
         </div>
         <MenuItems />
       </aside>
     </Fragment>
   );
 }
+
 export default AdminSideBar;
