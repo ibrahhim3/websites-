805840578b3ba88645c1c285ea478d7eb5eafff9
Sharef214 <m.nasit2021@gtu.edu.tr>
2024-11-23 23:25:31 +0300

    backend of cart

--- batas shop/server/models/Cart.js
+++ batas shop/server/models/Cart.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
-const Cart = new mongoose.Schema(
-    {
-        UserID : {
-            type : mongoose.Schema.Types.ObjectId ,
-            ref : "User" ,
-            required : true
-        } ,
-        Items : [ {
-            ProductID : {
-                type : mongoose.Schema.Types.ObjectId ,
-                ref : "Product" ,
-                required : true
-            } ,
-            Quantity : {
-                type : Number ,
-                required : true ,
-                min : 1
-            } ,
-        } ,
-        ]
-    } ,
-    { timestamps: true }
-) ;
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("Cart",Cart);
+module.exports = mongoose.model("Cart", CartSchema);
