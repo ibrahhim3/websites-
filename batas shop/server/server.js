4ecd97c9dd14ae0d77cfdb12caf0d8dca38f8b20
Sharef214 <m.nasit2021@gtu.edu.tr>
2024-12-03 22:55:07 +0300

    .

--- batas shop/server/server.js
+++ batas shop/server/server.js
@@ -7,7 +7,7 @@ const adminProductsRouter = require('./routes/admin/products-routes');
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
-
+const shopOrderRouter = require("./routes/shop/order-routes");
 
 
 
@@ -52,4 +52,6 @@ app.use('/api/admin/products', adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopAddressRouter);
+
 app.listen(PORT, ()=> console.log('Server is now running on port ${PORT}'));
