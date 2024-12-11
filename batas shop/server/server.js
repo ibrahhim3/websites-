48e2faaa2751de350c110b8bbd32933a3d840b56
Sharef214 <m.nasit2021@gtu.edu.tr>
2024-12-11 22:55:13 +0300

    backend done (without payment)

--- batas shop/server/server.js
+++ batas shop/server/server.js
@@ -11,9 +11,9 @@ const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
-
-
+const commonFeatureRouter = require("./routes/common/feature-routes");
 
 
 
@@ -60,5 +60,9 @@ app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
+
+
+app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(PORT, ()=> console.log('Server is now running on port ${PORT}'));
