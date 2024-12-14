2ee0d92fed89ea61e6fe0060c06054fd4e851f7c
ibr-al1 <i.al2020@gtu.edu.tr>
2024-12-14 22:13:36 +0300

    zz

--- batas shop/server/controllers/auth/auth-controller.js
+++ batas shop/server/controllers/auth/auth-controller.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res) => {
           userName: checkUser.userName,
         },
         "CLIENT_SECRET_KEY",
-        { expiresIn: "60m" }
+        { expiresIn: "120m" }
       );
   
       res.cookie("token", token, { httpOnly: true, secure: false }).json({
