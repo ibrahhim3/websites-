4c7dfd14e8ef673b28ec009df38166cf3a4d79e8
ibr-al1 <i.al2020@gtu.edu.tr>
2024-12-28 21:05:56 +0300

    ..

--- batas shop/client/src/App.jsx
+++ batas shop/client/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
-import VerifyCodePage from "./pages/auth/VerifyCodePage"
+import VerifyCodePage from "./pages/auth/verify"
 import AdminLayout from "./components/admin-view/layout";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
