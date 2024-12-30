69400d2076a3a1dd0c67d4b1887ffebfdbd46303
ibr-al1 <i.al2020@gtu.edu.tr>
2024-12-30 23:57:25 +0300

    email naming fixed

--- batas shop/server/helpers/nodemailer.js
+++ batas shop/server/helpers/nodemailer.js
@@ -1,9 +1,9 @@
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
 
 // Create Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', // You can replace this with another service like SendGrid or Outlook
+  service: "Gmail", // You can replace this with another service like SendGrid or Outlook
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -12,38 +12,35 @@ const transporter = nodemailer.createTransport({
 
 // Send Activation Email
 const sendActivationEmail = async (email, verificationCode) => {
- 
   const mailOptions = {
-    from: '"Your E-Commerce App" <your-email@gmail.com>',
+    from: '"BATAS SPICE SHOP" <batas.spice@gmail.com>',
     to: email,
-    subject: 'Account Verification Code',
+    subject: "Account Verification Code",
     html: `Your verification code is: ${verificationCode}. It will expire in 1 hour.`,
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log('Verification email sent');
+    console.log("Verification email sent");
   } catch (error) {
-    console.error('Error sending activation email:', error);
+    console.error("Error sending activation email:", error);
   }
 };
 
-
 const sendResetEmail = async (email, resetLink) => {
   const mailOptions = {
-    from: '"Your E-Commerce App" <your-email@gmail.com>',
+    from: '"BATAS SPICE SHOP" <batas.spice@gmail.com>',
     to: email,
-    subject: 'Password Reset Request',
+    subject: "Password Reset Request",
     html: `<p>Click the link below to reset your password:</p><a href="${resetLink}">${resetLink}</a>`,
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log('Password reset email sent');
+    console.log("Password reset email sent");
   } catch (error) {
-    console.error('Error sending password reset email:', error);
+    console.error("Error sending password reset email:", error);
   }
 };
 
 module.exports = { sendActivationEmail, sendResetEmail };
-
