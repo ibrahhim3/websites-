671d1cf27887dd835a7c5f7c8d1c64bbe9f4de85
ibr-al1 <i.al2020@gtu.edu.tr>
2025-01-13 20:09:28 +0300

    reviwes done

--- batas shop/client/src/components/shopping-view/product-details.jsx
+++ batas shop/client/src/components/shopping-view/product-details.jsx
@@ -1,4 +1,3 @@
-import { StarIcon } from "lucide-react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Dialog, DialogContent } from "../ui/dialog";
@@ -6,20 +5,22 @@ import { Separator } from "../ui/separator";
 import { Input } from "../ui/input";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
-import { useToast } from "@/hooks/use-toast";
 import { setProductDetails } from "@/store/shop/products-slice";
 import { Label } from "../ui/label";
 import StarRatingComponent from "../common/star-rating";
 import { useEffect, useState } from "react";
 import { addReview, getReviews } from "@/store/shop/review-slice";
+import { useToast } from "@/hooks/use-toast";
 
 function ProductDetailsDialog({ open, setOpen, productDetails }) {
-  const dispatch = useDispatch();
-  const { toast } = useToast();
-  const { user } = useSelector((state) => state.auth);
   const [reviewMsg, setReviewMsg] = useState("");
   const [rating, setRating] = useState(0);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.shopCart);
   const { reviews } = useSelector((state) => state.shopReview);
+
+  const { toast } = useToast();
 
   function handleRatingChange(getRating) {
     console.log(getRating, "getRating");
@@ -27,34 +28,8 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
     setRating(getRating);
   }
 
-  function handleAddReview() {
-    dispatch(
-      addReview({
-        productId: productDetails?._id,
-        userId: user?.id,
-        userName: user?.userName,
-        reviewMessage: reviewMsg,
-        reviewValue: rating,
-      })
-    ).then((data) => {
-      if (data.payload.success) {
-        setRating(0);
-        setReviewMsg("");
-        dispatch(getReviews(productDetails?._id));
-        toast({
-          title: "Review added successfully!",
-          style: {
-            backgroundColor: "white",
-            color: "black",
-          },
-          duration: 2000,
-        });
-      }
-    });
-  }
-
-  function handleAddToCart(getCurrentProductId) {
-    /*  let getCartItems = cartItems.items || [];
+  function handleAddToCart(getCurrentProductId, getTotalStock) {
+    let getCartItems = cartItems.items || [];
 
     if (getCartItems.length) {
       const indexOfCurrentItem = getCartItems.findIndex(
@@ -66,12 +41,17 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
           toast({
             title: `Only ${getQuantity} quantity can be added for this item`,
             variant: "destructive",
+            style: {
+              backgroundColor: "white",
+              color: "black",
+            },
+            duration: 2000,
           });
 
           return;
         }
       }
-    }*/
+    }
     dispatch(
       addToCart({
         userId: user?.id,
@@ -100,35 +80,63 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
     setReviewMsg("");
   }
 
+  function handleAddReview() {
+    dispatch(
+      addReview({
+        productId: productDetails?._id,
+        userId: user?.id,
+        userName: user?.userName,
+        reviewMessage: reviewMsg,
+        reviewValue: rating,
+      })
+    ).then((data) => {
+      if (data.payload.success) {
+        setRating(0);
+        setReviewMsg("");
+        dispatch(getReviews(productDetails?._id));
+        toast({
+          title: "Review added successfully!",
+          style: {
+            backgroundColor: "white",
+            color: "black",
+          },
+          duration: 2000,
+        });
+      }
+    });
+  }
+
   useEffect(() => {
     if (productDetails !== null) dispatch(getReviews(productDetails?._id));
   }, [productDetails]);
 
   console.log(reviews, "reviews");
 
+  const averageReview =
+    reviews && reviews.length > 0
+      ? reviews.reduce((sum, reviewItem) => sum + reviewItem.reviewValue, 0) /
+        reviews.length
+      : 0;
+
   return (
-    <Dialog open={open} onOpenChange={handleDialogClose}>
+    <Dialog open={open} onOpenChange={handleDialogClose} >
       <DialogContent className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw] bg-white overflow-y-auto max-h-[90vh]">
-        <div className="relative overflow-hidden rounded-lg">
+      <div className=" relative overflow-hidden rounded-lg">
           <img
             src={productDetails?.image}
             alt={productDetails?.title}
             width={600}
             height={600}
-            className="aspect-square w-full object-cover"
+            className="aspect-square w-full object-cover "
           />
         </div>
-
-        <div>
+        <div className="">
           <div>
-            <h1 className="text-3xl sm:text-2xl font-extrabold">
-              {productDetails?.title}
-            </h1>
+            <h1 className="text-3xl sm:text-2xl font-extrabold">{productDetails?.title}</h1>
             <p className="text-gray-500 text-lg sm:text-xl mb-5 mt-4">
               {productDetails?.description}
             </p>
           </div>
-
           <div className="flex items-center justify-between">
             <p
               className={`text-3xl font-bold text-primary ${
@@ -143,89 +151,63 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
               </p>
             ) : null}
           </div>
-
           <div className="flex item-center gap-2 mt-2">
             <div className="flex items-center gap-0.5">
-              <StarIcon className="w-5 h-5 fill-black" />
-              <StarIcon className="w-5 h-5 fill-black" />
-              <StarIcon className="w-5 h-5 fill-black" />
-              <StarIcon className="w-5 h-5 fill-black" />
-              <StarIcon className="w-5 h-5 fill-black" />
+              <StarRatingComponent rating={averageReview} />
             </div>
-            <span className="text-gray-500">(4.5)</span>
+            <span className="text-muted-foreground">
+              ({averageReview.toFixed(2)})
+            </span>
           </div>
-
           <div className="mt-5 mb-5">
-            <Button
-              onClick={() => handleAddToCart(productDetails?._id)}
+            {productDetails?.totalStock === 0 ? (
+              <Button className="w-full opacity-60 cursor-not-allowed">
+                Out of Stock
+              </Button>
+            ) : (
+              <Button
               className="w-full bg-red-600 hover:bg-red-700 text-black"
-            >
-              Add to Cart
-            </Button>
+                onClick={() =>
+                  handleAddToCart(
+                    productDetails?._id,
+                    productDetails?.totalStock
+                  )
+                }
+              >
+                Add to Cart
+              </Button>
+            )}
           </div>
-
           <Separator className="bg-gray-200" />
-
           <div className="max-h-[300px] overflow-auto">
             <h2 className="text-xl font-bold mb-4">Reviews</h2>
             <div className="grid gap-6">
-              <div className="flex gap-4">
-                <Avatar className="w-10 h-10 border">
-                  <AvatarFallback>SM</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <div className="flex items-center gap-2">
-                    <h3 className="font-bold">user name</h3>
+              {reviews && reviews.length > 0 ? (
+                reviews.map((reviewItem) => (
+                  <div className="flex gap-4">
+                    <Avatar className="w-10 h-10 border">
+                      <AvatarFallback>
+                        {reviewItem?.userName[0].toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid gap-1">
+                      <div className="flex items-center gap-2">
+                        <h3 className="font-bold">{reviewItem?.userName}</h3>
+                      </div>
+                      <div className="flex items-center gap-0.5">
+                        <StarRatingComponent rating={reviewItem?.reviewValue} />
+                      </div>
+                      <p className="text-muted-foreground">
+                        {reviewItem.reviewMessage}
+                      </p>
+                    </div>
                   </div>
-                  <div className="flex items-center gap-0.5">
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                  </div>
-                  <p className="text-gray-500">this is an awesome product</p>
-                </div>
-              </div>
-              <div className="flex gap-4">
-                <Avatar className="w-10 h-10 border">
-                  <AvatarFallback>SM</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <div className="flex items-center gap-2">
-                    <h3 className="font-bold">user name</h3>
-                  </div>
-                  <div className="flex items-center gap-0.5">
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                  </div>
-                  <p className="text-gray-500">this is an awesome product</p>
-                </div>
-              </div>
-              <div className="flex gap-4">
-                <Avatar className="w-10 h-10 border">
-                  <AvatarFallback>SM</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <div className="flex items-center gap-2">
-                    <h3 className="font-bold">user name</h3>
-                  </div>
-                  <div className="flex items-center gap-0.5">
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                    <StarIcon className="w-5 h-5 fill-black" />
-                  </div>
-                  <p className="text-gray-500">This is an awesome product</p>
-                </div>
-              </div>
+                ))
+              ) : (
+                <h1>No Reviews</h1>
+              )}
             </div>
-
-            <div className="mt-10 flex flex-col gap-2">
+            <div className="mt-10 flex-col flex gap-2">
               <Label>Write a review</Label>
               <div className="flex gap-1">
                 <StarRatingComponent
@@ -242,7 +224,8 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
               <Button
                 onClick={handleAddReview}
                 disabled={reviewMsg.trim() === ""}
-                className="bg-gray-700 text-white"
+                className="bg-gray-700 text-white "
+
               >
                 Submit
               </Button>
